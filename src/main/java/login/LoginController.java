b5f3130459bf82f7434bf84c7f49330a90e52d87
fadhilsrg <pratamafadil13@gmail.com>
2024-06-13 03:22:12 +0700

    Push Kedua Abang Ganteng

--- src/main/java/login/LoginController.java
+++ src/main/java/login/LoginController.java
@@ -1,19 +1,54 @@
 package login;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class LoginController {
+
     @FXML
     private TextField usernameField;
+
     @FXML
     private PasswordField passwordField;
 
     @FXML
-    private void handleLoginButtonAction() {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        // Implement login logic here
+    private Button loginButton;
+
+    @FXML
+    private Label forgotPasswordLabel;
+
+    @FXML
+    private Label libraryLabel;
+
+    @FXML
+    private Label loginLabelKata;
+
+    @FXML
+    private Label loginLabel;
+
+    @FXML
+    private Label caturLabel;
+
+    public void initialize() {
+        animateTextFill();
+    }
+
+    private void animateTextFill() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(caturLabel.textFillProperty(), Color.RED)),
+                new KeyFrame(Duration.seconds(1), new KeyValue(caturLabel.textFillProperty(), Color.GREEN)),
+                new KeyFrame(Duration.seconds(2), new KeyValue(caturLabel.textFillProperty(), Color.BLUE)),
+                new KeyFrame(Duration.seconds(3), new KeyValue(caturLabel.textFillProperty(), Color.RED))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 }
