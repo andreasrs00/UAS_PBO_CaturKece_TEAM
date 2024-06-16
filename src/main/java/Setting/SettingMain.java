d11c19817b0c13920cd2bac1152d388fce488b22
anggardaa muhammad aziz <anggardamaziz04@gmail.com>
2024-06-16 21:34:39 +0700

    setting ni bro ahhhh

---
+++ src/main/java/Setting/SettingMain.java
@@ -0,0 +1,30 @@
+package Setting;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SettingMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Setting/Setting.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        // Load CSS file if needed
+        scene.getStylesheets().add(getClass().getResource("/Setting/Setting.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Chess Game Settings");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,34 +1,26 @@
 module caturkece.com.uas_pbo_caturkece_team {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-    requires java.sql;
+        requires javafx.controls;
+        requires javafx.fxml;
+//        requires javafx.web;
+        requires java.sql;
+        requires javafx.media;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
-    opens login to javafx.fxml;
-    exports login;
-
-// <<<<<<< alvian
-//     opens Database.Controller to javafx.fxml;
-//     opens Database.Model to javafx.graphics, javafx.fxml;
-//     exports Database;
+//        requires org.controlsfx.controls;
+//        requires com.dlsc.formsfx;
+//        requires net.synedra.validatorfx;
+//        requires org.kordamp.ikonli.javafx;
+//        requires org.kordamp.bootstrapfx.core;
+//        requires eu.hansolo.tilesfx;
+//        requires com.almasb.fxgl.all;
 
 
+        opens login to javafx.fxml;
+        exports login;
 
+        opens Setting to javafx.fxml;
+        exports Setting;
 
-
-
-
-
-
-    opens menu to javafx.fxml;
-    exports menu;
+        opens menu to javafx.fxml;
+        exports menu;
 
 }
