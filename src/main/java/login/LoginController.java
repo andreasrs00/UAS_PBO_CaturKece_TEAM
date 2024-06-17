de37dc39267ed82b354ae438f02e3f0ac944f9d7
alvian <alvianrmli@gmail.com>
2024-06-17 14:39:54 +0700

    tralalalla

--- src/main/java/login/LoginController.java
+++ src/main/java/login/LoginController.java
@@ -1,5 +1,6 @@
 package login;
 
+//import library javafx
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,8 +10,15 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+//import package
+import menu.MenuApplication;
+import Database.DatabaseConnection;
+
+
+//java.sql
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -47,9 +55,7 @@ public class LoginController {
     @FXML
     private Label errorMessageLabel;
 
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/db_catur";
-    private static final String DB_USERNAME = "postgres";
-    private static final String DB_PASSWORD = "password";
+
 
     public void initialize() {
 
@@ -69,37 +75,60 @@ public class LoginController {
         timeline.play();
     }
 
+
+
+
+
     @FXML
     private void handleLogin() {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
         if (isLoginValid(username, password)) {
-            // Login berhasil
             errorMessageLabel.setVisible(false);
             System.out.println("Login berhasil");
-            // Anda bisa melakukan navigasi ke scene berikutnya atau tindakan lainnya di sini
+            openMenuPage();
+
+
+
         } else {
-            // Login gagal
             errorMessageLabel.setText("Username atau password salah");
             errorMessageLabel.setVisible(true);
             System.out.println("login gagal");
+            System.out.println("Username atau password salah");
         }
     }
 
-    private boolean isLoginValid(String username, String password) {
+    private void openMenuPage() {
+        try {
+
+
+            MenuApplication menuApp = new MenuApplication();
+            Stage menuStage = new Stage();
+            menuApp.start(menuStage);
+            System.out.println(menuStage);
+
+            Stage loginStage = (Stage) usernameField.getScene().getWindow();
+
+            loginStage.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        }
+
+
+        private boolean isLoginValid(String username, String password) {
         String query = "SELECT * FROM users WHERE username = ? AND password = ?";
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                return resultSet.next(); // Jika ada hasil dari query, berarti login berhasil
+                return resultSet.next();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
     }
-
 }
